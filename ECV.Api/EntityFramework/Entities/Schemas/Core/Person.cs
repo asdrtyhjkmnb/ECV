bd5dad73de74568fed977cf3d338e6744683df8b
Константин <nazarov-ks@narod.ru>
2021-10-29 22:21:03 +0300

    Установление связи с БД Postgres

---
+++ ECV.Api/EntityFramework/Entities/Schemas/Core/Person.cs
@@ -0,0 +1,10 @@
+﻿namespace ECV.Api.EntityFramework.Entities.Schemas.Core
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string LastName { get; set; }
+        public string? MiddleName { get; set; }
+        public string FirstName { get; set; }
+    }
+}
---
+++ ECV.Api/EntityFramework/Migrations/20211029191843_Initial.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace ECV.Api.EntityFramework.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Persons",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    LastName = table.Column<string>(type: "text", nullable: false),
+                    MiddleName = table.Column<string>(type: "text", nullable: true),
+                    FirstName = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Persons", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Persons");
+        }
+    }
+}
